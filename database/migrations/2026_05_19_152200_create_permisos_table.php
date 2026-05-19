1cc00ae7ab80829b907d0258cfd563bcb3850516
AlbertoPDM30 <albertodpm2000@gmail.com>
2026-05-19 11:47:10 -0400

    created migrations: roles, permisos, roles_permisos

---
+++ database/migrations/2026_05_19_152200_create_permisos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permisos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_tecnico')->unique();
+            $table->string('modulo')->required();
+            $table->string('accion')->required();
+            $table->string('descripcion')->required();
+            $table->timestamps();
+            // $table->timestamps('created_at');
+            // $table->timestamps('updated_at')->default('CURRENT_TIMESTAMP');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permisos');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        // Validar la existencia de la BD antes de ejecutar los seeders
+        $database = Config::get('database.connections.mysql.database');
+        DB::statement("CREATE DATABASE IF NOT EXISTS `$database`");
+        
         // User::factory(10)->create();
 
         User::factory()->create([
